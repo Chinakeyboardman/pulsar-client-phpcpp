7abd66950e3ba8aff2807c2b3fb683d671d35c79
chenjiawei <chenjiawei@nineuniverse.com>
2022-07-02 23:29:11 +0800

    solve the problem "producer->send not any return";

--- src/Producer.h
+++ src/Producer.h
@@ -7,10 +7,12 @@ class Producer : public Php::Base {
 
   public:
     pulsar::Producer producer;
+    pulsar::Result result;
 
     Producer() {}
 
-    void send(Php::Parameters &params);
+    // void send(Php::Parameters &params);
+    Php::Value send(Php::Parameters &params);
 };
 
 void registerProducer(Php::Namespace &pulsarNamespace);
